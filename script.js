eeaab795f43f787f30dc996abefdb20a85a63271
chaurasiavipin <vipinchaurasia481@gmail.com>
2025-05-18 23:26:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
+	let ans=document.querySelectorAll(".price");
+	let sum=0;
+ ans.forEach(el => {
+    sum += parseInt(el.textContent);
+  });
+
 //Add your code here
   
 };
